9a98c56bd13f781e0951580c7337ce4e8fd98e01
Dave Medina <isra2dave@gmail.com>
2022-04-03 17:11:45 -0300

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import Home from './views/Home';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./views/Home";
+import Chat from "./views/Chat";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
-
-    <Route path="/" element={<Home/>}>
-    </Route>
-        </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="chat" element={<Chat />} />
+      </Route>
+    </Routes>
   );
 }
 
